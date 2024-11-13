6d8b07f86308711cc932b882ee57e66968c278c1
leko-szabolcs <szabolcsworks@gmail.com>
2024-11-13 09:13:30 +0100

    Létrehoztam a PárhuzamosController vezérlő osztályt és FXML annotáció segítségével létrehoztam a start gombot, valamint a két címkét, hogy később felhasználjam őket a párhuzamos programozás bemutatásához. Az fxml fájlban, pedig hozzákapcsoltam a Controllerben difiniált annotációkat a felületi elemekkel.

---
+++ utazas-beadando-javafx/src/main/java/com/example/utazasbeadandojavafx/ParhuzamosController.java
@@ -0,0 +1,17 @@
+package com.example.utazasbeadandojavafx;
+
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+
+
+public class ParhuzamosController {
+
+    @FXML private Button startButton;
+    @FXML private Label thread_one_txt;
+    @FXML private Label thread_two_txt;
+
+
+}
