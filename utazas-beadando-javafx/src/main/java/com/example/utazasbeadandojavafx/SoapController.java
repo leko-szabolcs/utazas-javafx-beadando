82f85f4eb5a5b7fcb9af213b3b02db18d3c1126b
leko-szabolcs <szabolcsworks@gmail.com>
2024-11-20 10:26:45 +0100

    Létrehoztam egy vezérlő osztályt, ahol megadtam a egy gombot és címkét. A gomb segítségével lehet majd beolvasni a bekért adatokat a java programmal készűlt .JAR fájlból. A címke fogja jelezni a betöltés állapotát (sikeres, sikertelen). Valamint változtattam a .fxml fájlon. Egy betöltés és egy grafikon almenüt hoztam létre a későbbiekhez.

---
+++ utazas-beadando-javafx/src/main/java/com/example/utazasbeadandojavafx/SoapController.java
@@ -0,0 +1,13 @@
+package com.example.utazasbeadandojavafx;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class SoapController {
+    @FXML private Button loadFileButton;
+
+    @FXML
+    private Label statusLabel;
+
+}
